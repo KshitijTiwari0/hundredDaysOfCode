42a61928c55751282f92d474d1894c7118269a6b
KshitijTiwari0 <satyarupkshitij3002@gmail.com>
2024-06-23 15:14:06 +0530

    day 2 updated
    power of two problem

---
+++ day2/231UsingRecursion.c
@@ -0,0 +1,16 @@
+bool isPowerOfTwo(int n) {
+    if(n<1){
+        return false;
+    }
+    else if(n == 1)
+    {
+        return true;
+    }
+    if(n%2 == 0)
+    {
+        return isPowerOfTwo(n/2);
+    }
+    else{
+        return false;
+    }
+}
